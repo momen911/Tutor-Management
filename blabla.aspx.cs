b3f4bc8e24fb7feb4883596d7374b9a7d96d64c9
SabbirKhan647 <sabbirkhan647@gmail.com>
2014-03-20 11:15:20 -0400

    Done

--- blabla.aspx.cs
+++ blabla.aspx.cs
@@ -5,10 +5,10 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
-public partial class Activities : System.Web.UI.Page
+public partial class blabla : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        //jfhsjdkh
+
     }
 }
